7bf39979a90b04571af3295dbafd8e1e49068039
AntonioCandido31 <antoniocandidodacostaribeiro@gmail.com>
2026-08-21 14:09:54 -0300

    implementei a calculadora

---
+++ calculadora.py
@@ -0,0 +1,23 @@
+import soma
+import subtrai
+import multiplica
+import divide
+
+def main():
+  n1 = float(input('Digite o primeiro número: '))
+  n2 = float(input("Digite o segundo número: "))
+  operador = input("Digite o operador (+, -, *, /): ")
+    if operador == "+":
+        resultado = soma.somaf(n1, n2)
+    elif operador == "-":
+        resultado = subtrai.subtraif(n1, n2)
+    elif operador == "*":
+        resultado = multiplica.multiplicaf(n1, n2)
+    elif operador == "/":
+        resultado = divide.dividef(n1, n2)
+    else:
+        print("Operador inválido.")
+        return
+
+if __name__ == "__main__":
+    main()
